ee8f28627469dd343d0fd90a656a6aa21cae966a
Paco <frjafopa@gmail.com>
2026-04-09 07:46:06 -0600

    feat: IP Logger (#657)
    * feat: IP Logger
    
    Create a log to track IPs of characters
    This way you can check who is who and enforce your server rules with better precision.
    
    * Update Logger
    
    It's now configurable at config.lua
    
    * Lua code format - (Stylua)
    
    * Update config.lua.dist
    
    ---------
    
    Co-authored-by: GitHub Actions <github-actions[bot]@users.noreply.github.com>

--- src/config/configmanager.cpp
+++ src/config/configmanager.cpp
@@ -125,6 +125,7 @@ bool ConfigManager::load() {
 	loadBoolConfig(L, ENABLE_OFFLINE_TRAINING, "enableOfflineTraining", true);
 	loadBoolConfig(L, ENABLE_MARKET, "enableMarket", true);
 	loadBoolConfig(L, ADVANCED_PARTY_PROTECTION, "advancedPartyProtection", true);
+	loadBoolConfig(L, LOG_PLAYER_IP, "logPlayerIP", true);
 	loadBoolConfig(L, RATE_USE_STAGES, "rateUseStages", false);
 	loadBoolConfig(L, REFUND_BEGINNING_WEAPON_MANA, "refundBeginningWeaponMana", false);
 	loadBoolConfig(L, REMOVE_BEGINNING_WEAPON_AMMO, "removeBeginningWeaponAmmunition", true);
