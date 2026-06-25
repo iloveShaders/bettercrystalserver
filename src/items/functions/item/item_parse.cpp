d10cbc1629fba4012fe4cd2eecc396c82d4d40df
Kakeriel <73860497+iloveShaders@users.noreply.github.com>
2026-06-26 00:48:23 +0200

    fix by Jattartajar: Weapon Melee Attack Effect

--- src/items/functions/item/item_parse.cpp
+++ src/items/functions/item/item_parse.cpp
@@ -92,6 +92,7 @@ void ItemParse::initParse(const std::string &stringValue, pugi::xml_node attribu
 	ItemParse::parseElementalBond(stringValue, valueAttribute, itemType);
 	ItemParse::parseMantra(stringValue, valueAttribute, itemType);
 	ItemParse::parseAmmoSaveChance(stringValue, valueAttribute, itemType);
+	ItemParse::parseMeleeAttackEffect(stringValue, valueAttribute, itemType);
 }
 
 void ItemParse::parseDummyRate(pugi::xml_node attributeNode, ItemType &itemType) {
@@ -1323,5 +1324,33 @@ void ItemParse::parseAmmoSaveChance(const std::string &stringValue, pugi::xml_at
 			value = 100;
 		}
 		itemType.ammoSaveChance = static_cast<uint8_t>(value);
+	}
+}
+
+void ItemParse::parseMeleeAttackEffect(const std::string &stringValue, pugi::xml_attribute valueAttribute, ItemType &itemType) {
+	if (stringValue != "meleeattackeffect") {
+		return;
+	}
+
+	static const phmap::flat_hash_map<std::string, MagicEffectClasses> meleeAttackEffects = {
+		{ "sword", CONST_ME_SWORD_ATTACK },
+		{ "swordattack", CONST_ME_SWORD_ATTACK },
+		{ "club", CONST_ME_CLUB_ATTACK },
+		{ "clubattack", CONST_ME_CLUB_ATTACK },
+		{ "axe", CONST_ME_AXE_ATTACK },
+		{ "axeattack", CONST_ME_AXE_ATTACK },
+		{ "fist", CONST_ME_FIST_ATTACK },
+		{ "fistattack", CONST_ME_FIST_ATTACK },
+		{ "monkstaff", CONST_ME_MONK_STAFF_ATTACK },
+		{ "monkstaffattack", CONST_ME_MONK_STAFF_ATTACK },
+		{ "monkdaggers", CONST_ME_MONK_DAGGERS_ATTACK },
+		{ "monkdaggersattack", CONST_ME_MONK_DAGGERS_ATTACK },
+	};
+
+	const auto effect = meleeAttackEffects.find(asLowerCaseString(valueAttribute.as_string()));
+	if (effect != meleeAttackEffects.end()) {
+		itemType.meleeAttackEffect = effect->second;
+	} else {
+		g_logger().warn("[Items::parseItemNode] - Unknown meleeAttackEffect {}", valueAttribute.as_string());
 	}
 }
--- src/items/functions/item/item_parse.hpp
+++ src/items/functions/item/item_parse.hpp
@@ -170,6 +170,7 @@ const phmap::flat_hash_map<std::string, ItemParseAttributes_t> ItemParseAttribut
 	{ "elementalbond", ITEM_PARSE_ELEMENTALBOND },
 	{ "mantra", ITEM_PARSE_MANTRA },
 	{ "ammosavechance", ITEM_PARSE_AMMOSAVECHANCE },
+	{ "meleeattackeffect", ITEM_PARSE_MELEEATTACKEFFECT },
 };
 
 const phmap::flat_hash_map<std::string, ItemTypes_t> ItemTypesMap = {
@@ -340,6 +341,7 @@ private:
 	static void parseHouseRelated(std::string_view stringValue, pugi::xml_attribute valueAttribute, ItemType &itemType);
 	static void parseUnscriptedItems(std::string_view stringValue, pugi::xml_node attributeNode, pugi::xml_attribute valueAttribute, ItemType &itemType);
 	static void parseMantra(const std::string &stringValue, pugi::xml_attribute valueAttribute, ItemType &itemType);
+	static void parseMeleeAttackEffect(const std::string &stringValue, pugi::xml_attribute valueAttribute, ItemType &itemType);
 	static void parseElementalBond(const std::string &stringValue, pugi::xml_attribute valueAttribute, ItemType &itemType);
 	static void parseAmmoSaveChance(const std::string &stringValue, pugi::xml_attribute valueAttribute, ItemType &itemType);
 
