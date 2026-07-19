844525f89960800daaeb73810ec7b547c2129163
Kakeriel <73860497+iloveShaders@users.noreply.github.com>
2026-07-19 22:58:14 +0200

    fix: cd reduction perk #831 by GuiValladao

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -12927,7 +12927,7 @@ void Player::applyEquippedWeaponProficiency(const uint16_t itemId) {
 				continue;
 			}
 
-			if (perk.perkValue < 0.0f) {
+			if (perk.perkValue < 0.0f && perk.perkType != PROFICIENCY_PERK_AUGMENT_TYPE) {
 				continue;
 			}
 
