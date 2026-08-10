aa9e3334d7bd194e75f617532a4ca3925778940c
Kakeriel <73860497+iloveShaders@users.noreply.github.com>
2026-08-10 03:26:41 +0200

    feat: Add configurable Lua debug hook- #857

--- src/config/configmanager.cpp
+++ src/config/configmanager.cpp
@@ -111,6 +111,7 @@ bool ConfigManager::load() {
 	loadBoolConfig(L, HOUSE_PURSHASED_SHOW_PRICE, "housePurchasedShowPrice", false);
 	loadBoolConfig(L, INVENTORY_GLOW, "inventoryGlowOnFiveBless", false);
 	loadBoolConfig(L, LOYALTY_ENABLED, "loyaltyEnabled", true);
+	loadBoolConfig(L, LUA_DEBUG_HOOK, "luaDebugHook", true);
 	loadBoolConfig(L, MARKET_PREMIUM, "premiumToCreateMarketOffer", true);
 	loadBoolConfig(L, METRICS_ENABLE_OSTREAM, "metricsEnableOstream", false);
 	loadBoolConfig(L, METRICS_ENABLE_PROMETHEUS, "metricsEnablePrometheus", false);
