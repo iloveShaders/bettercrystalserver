1c550561abfb47f65f9b3cb9f5e9b4862cb4f5c5
Kakeriel <73860497+iloveShaders@users.noreply.github.com>
2026-08-10 13:29:51 +0200

    fix: Tibia Coins cannot be offered on the market (resource 0x5B never sent) #868

--- src/server/network/protocol/protocolgame.cpp
+++ src/server/network/protocol/protocolgame.cpp
@@ -6196,6 +6196,11 @@ void ProtocolGame::sendCoinBalance() {
 	}
 
 	writeToOutputBuffer(msg);
+
+	if (!oldProtocol) {
+		sendResourceBalance(RESOURCE_COIN_NORMAL, player->coinBalance);
+		sendResourceBalance(RESOURCE_COIN_TRANSFERRABLE, player->coinTransferableBalance);
+	}
 }
 
 void ProtocolGame::updateCoinBalance() {
